874523ca7655ddbc5312970a14f7cc30370d4714
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-03-08 13:33:32 -0500

    add both vatican and human rights items.

--- src/constants/ItemActionTypes.jsx
+++ src/constants/ItemActionTypes.jsx
@@ -3,7 +3,8 @@ var keyMirror = require('keymirror');
 var ItemActionTypes = keyMirror({
   ITEM_SET_ITEM: null,
   ITEM_SHOW_WINDOW: false,
-  PRE_LOAD_ITEMS: false,
+  PRE_LOAD_VATICAN_ITEMS: false,
+  PRE_LOAD_HUMANRIGHTS_ITEMS: false,
 });
 
 module.exports = ItemActionTypes;
