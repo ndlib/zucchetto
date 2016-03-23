bc3b75aed9cc0a1c6a12496b5e9cb3f5e60a3718
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-03-23 08:56:48 -0400

    fix bug with pprd

--- src/modules/HoneycombURL.js
+++ src/modules/HoneycombURL.js
@@ -1,9 +1,9 @@
 
 module.exports = function() {
   if(process.env.NODE_ENV == 'production') {
-    return 'http://honeycomb.library.nd.edu';
+    return 'https://honeycomb.library.nd.edu';
   } else if (process.env.NODE_ENV == 'preproduction') {
-    return 'http://honeycombpprd-vm.library.nd.edu';
+    return 'https://honeycombpprd-vm.library.nd.edu';
   } else {
     return 'http://localhost:3017';
   }
