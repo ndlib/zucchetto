d3b4bda59d70675403cedd9ddd24555a98de810f
Justin Gondron <jgondron@nd.edu>
2016-03-28 15:06:33 -0400

    no message

---
+++ src/routes/DocumentPage.jsx
@@ -0,0 +1,46 @@
+'use strict'
+import React, { Component, PropTypes } from 'react';
+import mui from 'material-ui';
+
+import Header from '../components/StaticAssets/Header.jsx';
+import Document from '../components/Document/Document.jsx';
+import ItemActions from '../actions/ItemActions.jsx'
+import ItemStore from '../store/ItemStore.js'
+
+class DocumentPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loaded: false,
+    };
+    this.preLoadFinished = this.preLoadFinished.bind(this)
+  }
+
+  componentWillMount() {
+    ItemActions.preLoadItems();
+    ItemStore.on("PreLoadFinished", this.preLoadFinished);
+  }
+
+  preLoadFinished() {
+    this.setState({ loaded: true });
+  }
+
+  renderDocument() {
+    if (this.state.loaded) {
+      return (<Document documentId={ this.props.params.id } />);
+    } else {
+      return (<p>Loading....</p>);
+    }
+  }
+
+  render() {
+    return (
+      <mui.Paper zDepth={ 0 }>
+        <Header/>
+        { this.renderDocument() }
+      </mui.Paper>
+    );
+  }
+}
+
+export default DocumentPage;
