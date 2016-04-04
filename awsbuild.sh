5f6cbc11bf1b38b3c84cda23892f4ddf042f5cbd
Robert Fox <rfox2@nd.edu>
2016-04-04 15:01:54 -0400

    DEC-892 Corrected path reference

--- awsbuild.sh
+++ awsbuild.sh
@@ -3,10 +3,10 @@ PROJECT=csthr
 BUCKET=${PROJECT}.library.nd.edu
 ENV_ALIAS=honeycombpprd-vm
 
-cd ./public/cache_data
+cd ./public
 
-curl -o cst_data.json https://${ENV_ALIAS}.library.nd.edu/v1/collections/vatican/items
-curl -o ihrl_data.json https://${ENV_ALIAS}library.nd.edu/v1/collections/humanrights/items
+curl -o ./cache_data/cst_data.json https://${ENV_ALIAS}.library.nd.edu/v1/collections/vatican/items
+curl -o ./cache_data/ihrl_data.json https://${ENV_ALIAS}library.nd.edu/v1/collections/humanrights/items
 
 aws s3 mb s3://${BUCKET}
 aws s3 website s3://${BUCKET} --index-document index.html --error-document index.html
