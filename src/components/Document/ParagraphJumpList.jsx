f9a31160fdcfddb09cc92817798ee460a880a233
Dan Wolfe <dwolfe2@nd.edu>
2016-04-05 10:26:03 -0400

    replace name with metadata.name.values[0].value

--- src/components/Document/ParagraphJumpList.jsx
+++ src/components/Document/ParagraphJumpList.jsx
@@ -30,7 +30,7 @@ class ParagraphJumpList extends Component {
           <option
             key={ item.id }
             value={ item.id }
-          >{ item.name }
+          >{ item.metadata.name.values[0].value }
         </option>
         );
       }
