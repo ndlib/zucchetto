027e6b507578691c24088601dff062fc97a788f1
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-04-07 14:59:02 -0400

    fix some warnings.

--- src/components/Notebook/NotebookColumn.jsx
+++ src/components/Notebook/NotebookColumn.jsx
@@ -29,7 +29,7 @@ class NotebookColumn extends Component {
 }
 
 NotebookColumn.propTypes = {
-  item: React.PropTypes.object.isRequired,
+  item: React.PropTypes.object,
 }
 
 NotebookColumn.defaultProps = {
