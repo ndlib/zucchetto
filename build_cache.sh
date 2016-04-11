cc12a12c9c042840477e0492d3cddd7f9b01efb1
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-04-11 10:24:18 -0400

    fix to work if there is no cache dir.

--- build_cache.sh
+++ build_cache.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ENV_ALIAS=honeycomb
 
+mkdir ./public/resources/cache_data
+
 curl -o ./public/resources/cache_data/cst_data.json https://${ENV_ALIAS}.library.nd.edu/v1/collections/vatican/items
 curl -o ./public/resources/cache_data/ihrl_data.json https://${ENV_ALIAS}.library.nd.edu/v1/collections/humanrights/items
