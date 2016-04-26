f6179fca7a20073a95f7204668f82412d9d33b3d
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-04-26 12:15:59 -0400

    Added a link to the project feedback form.

--- awsbuild.sh
+++ awsbuild.sh
@@ -5,7 +5,6 @@ BUCKET=${PROJECT}.library.nd.edu
 ENV_ALIAS=honeycomb
 
 git pull
-
 npm run build
 
 cd ./public
@@ -17,6 +16,6 @@ aws s3 mb s3://${BUCKET}
 aws s3 website s3://${BUCKET} --index-document index.html --error-document index.html
 aws s3 sync . s3://${BUCKET} --exclude '.*' --exclude '*.md' --delete --acl public-read
 
-npm run build-dev
+# npm run build-dev
 
 echo ${BUCKET}.s3-website-us-east-1.amazonaws.com
--- src/components/StaticAssets/Footer.jsx
+++ src/components/StaticAssets/Footer.jsx
@@ -2,17 +2,21 @@
 import React, { Component, PropTypes } from 'react';
 import CompareButton from '../Document/CompareButton.jsx';
 import BackToSearchButton from '../Document/BackToSearchButton.jsx';
+import { Link } from 'react-router';
 
 class Footer extends Component {
   render() {
     return (
       <footer>
         <div className="row">
-          <div className="col-sm-6 compare">
+          <div className="col-sm-5 compare">
             { this.props.showCompareButton ? (<CompareButton/>) : '' }
             { this.props.showBackToSearchButton ? (<BackToSearchButton/>) : '' }
           </div>
-          <div className="col-sm-6 right">
+          <div className="col-sm-2">
+            <a href="https://docs.google.com/a/nd.edu/forms/d/1yCnSjl4nBCJYmw70_S2VfVx1LzgNQ-kmroOqapq6i0Q/viewform" target="_blank">Project Feedback</a>
+          </div>
+          <div className="col-sm-5 right">
             <a href="http://humanrights.nd.edu/" target="_blank" ><img src="/resources/images/cchr.png" alt="The Center For Civil and Human Rights" className="cchr" /></a>
             <a href="https://library.nd.edu/cds" target="_blank" ><img src="/resources/images/undhl-cds.png" alt="Hesburgh Library Center for Digital Scholarship University of Notre Dame" className="undhl" /></a>
           </div>
