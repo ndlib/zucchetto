bfe18480958927f7619d6697e2245a1ddc0800a5
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-04-28 13:51:51 -0400

    remove red

--- awsbuild.sh
+++ awsbuild.sh
@@ -16,6 +16,6 @@ aws s3 mb s3://${BUCKET}
 aws s3 website s3://${BUCKET} --index-document index.html --error-document index.html
 aws s3 sync . s3://${BUCKET} --exclude '.*' --exclude '*.md' --delete --acl public-read
 
-# npm run build-dev
+npm run build-dev
 
 echo ${BUCKET}.s3-website-us-east-1.amazonaws.com
