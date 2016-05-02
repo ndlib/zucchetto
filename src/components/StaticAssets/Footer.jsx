562a0c6fbf6210f3e845bccb87137dd0248efbdb
Andy Wetherill <awetheri@nd.edu>
2016-05-02 15:06:12 -0400

    Added footer links and created feedback button for header.

--- src/components/StaticAssets/Footer.jsx
+++ src/components/StaticAssets/Footer.jsx
@@ -14,10 +14,11 @@ class Footer extends Component {
             { this.props.showBackToSearchButton ? (<BackToSearchButton/>) : '' }
           </div>
           <div className="col-sm-2">
-            <a href="https://docs.google.com/a/nd.edu/forms/d/1yCnSjl4nBCJYmw70_S2VfVx1LzgNQ-kmroOqapq6i0Q/viewform" target="_blank">Project Feedback</a>
+            <a href="https://docs.google.com/a/nd.edu/forms/d/1yCnSjl4nBCJYmw70_S2VfVx1LzgNQ-kmroOqapq6i0Q/viewform"  className="feedback" target="_blank">Project Feedback<br /></a>
+            <span className="tearms"><a href="#">Tearms of Service</a> | <a href="#">Privacy Policy</a></span>
           </div>
           <div className="col-sm-5 right">
-            <a href="http://humanrights.nd.edu/" target="_blank" ><img src="/resources/images/cchr.png" alt="The Center For Civil and Human Rights" className="cchr" /></a>
+            <a href="http://humanrights.nd.edu/" target="_blank"><img src="/resources/images/cchr.png" alt="The Center For Civil and Human Rights" className="cchr" /></a>
             <a href="https://library.nd.edu/cds" target="_blank" ><img src="/resources/images/undhl-cds.png" alt="Hesburgh Library Center for Digital Scholarship University of Notre Dame" className="undhl" /></a>
           </div>
         </div>
