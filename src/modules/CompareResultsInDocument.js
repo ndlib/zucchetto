7838e1d1a4586dd0bb64801cd58a4c9f7b120bcd
Dan Wolfe <dwolfe2@nd.edu>
2016-05-04 14:57:36 -0400

    remove console.log

--- src/modules/CompareResultsInDocument.js
+++ src/modules/CompareResultsInDocument.js
@@ -1,14 +1,10 @@
 import ItemStore from '../store/ItemStore.js';
-
 module.exports = function(document, compareList) {
   var count = 0;
-  console.log(document);
-  console.log(compareList)
   for (var i=0; i < compareList.length; i++) {
     if(ItemStore.getItemParent(ItemStore.getItem(compareList[i])) == document) {
       count++;
     }
   }
-
   return count;
 }
