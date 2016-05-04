4fda5d1f57f7b61785348031691390be79c91336
Dan Wolfe <dwolfe2@nd.edu>
2016-05-04 13:39:22 -0400

    First pass at rendering metadata.

--- src/components/Document/MetadataSection.jsx
+++ src/components/Document/MetadataSection.jsx
@@ -1,16 +1,44 @@
 'use strict'
 import React, { Component, PropTypes } from 'react';
+import mui, { Paper } from 'material-ui';
 
 class MetadataSection extends Component {
 
   constructor(props) {
     super(props);
+    this._metadata = this.props.document.metadata;
   }
 
+
   render() {
-    console.log(this.props.document)
+    var data = [];
+
+    for (var prop in this._metadata) {
+      data.push({label: this._metadata[prop].label.toLowerCase(), value: (<p key={ prop } >{this._metadata[prop].label}: {this._metadata[prop].values[0].value}</p>)});
+    }
+
+    data = data.sort(function(a,b) {
+      if (a.label < b.label)
+       return -1
+      if (a.label > b.label)
+       return 1
+      return 0 //default return value (no sorting)
+    });
+
+    var values = []
+    for(var item in data) {
+      values.push(data[item].value);
+    }
+
     return (
-       <p/>
+      <Paper>
+        <Paper zDepth={ 0 } style={{ width: "100%" }}>
+          <h2 className='heading1'>Metadata</h2>
+          <div style={{ margin: '0 auto', maxWidth: '60vw' }}>
+            {values}
+          </div>
+        </Paper>
+      </Paper>
      );
   }
 }
