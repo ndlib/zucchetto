414858d7933fcd2fef2b4dfa57e0a49fc8f52ac9
Andy Wetherill <awetheri@nd.edu>
2016-05-05 13:03:45 -0400

    Added privacy policy page and a few text edits.

---
+++ src/components/StaticPages/Privacy.jsx
@@ -0,0 +1,87 @@
+'use strict'
+var React = require('react');
+var Header = require('../StaticAssets/Header.jsx');
+var Navigation = require('../StaticAssets/Navigation.jsx');
+var Footer = require('../StaticAssets/Footer.jsx');
+var Privacy = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <Header/>
+        <div className="row body">
+          	<div className="col-sm-3 left-col">
+          		<Navigation/>
+          	</div>
+          	<div className="col-sm-9 right-col">
+        		<h1>Privacy Policy – last modified 21 April 2016</h1>
+<p>Your privacy is important to us. This privacy policy (“Policy”) explains how your personal information is collected, used, and disclosed by the Catholic Social Teaching and International Human Rights Comparative Database Project (the “Project”), in the Center for Civil and Human Rights at the University of Notre Dame.</p>
+
+<h2>WHAT INFORMATION DO WE COLLECT?</h2>
+<p>We do not ask you to register to use our site and your visit will be anonymous.</p>
+<p>We will collect personally identifiable information when you provide it to us (for example if you report an error to us using the relevant form).</p>
+<p>The Project uses Google Analytics to gather statistics for portions of the website. The information gathered will be used to improve services for users. Google Analytics uses a browser cookie for statistical analysis related to your browsing behavior on the website. If you choose, you can opt out by turning off cookies in the preferences settings in your browser, or download and install Google Analytics Opt-out Browser Add-on at http://tools.google.com/dlpage/gaoptout. For more information on Google Analytics, please visit <a href="https://www.google.com/analytics/" target="_blank">https://www.google.com/analytics/</a>.</p>
+
+<h2>DO WE USE COOKIES?</h2>
+<p>We will use cookies to enhance the functionality of the site.
+</p>
+<h2>DO WE DISCLOSE ANY INFORMATION TO OUTSIDE PARTIES?</h2>
+<p>We do not generally sell, trade, or otherwise transfer personally identifiable information to outside parties. However we may release information when we believe release is appropriate to comply with the law, enforce our site policies, or protect our or other’s rights, property, or safety.</p>
+<p>Non-personally identifiable visitor information may be provided to other parties for other uses.</p>
+
+<h2>THIRD PARTY LINKS</h2>
+<p>At our discretion, we may include links to other websites or offers of other products or services on our website. Any third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites.</p>
+
+<h2>TERMS AND CONDITIONS</h2>
+<p>Please also visit our Terms and Conditions section establishing the use, disclaimers, and limitations of liability governing the use of our website.</p>
+<h2>YOUR CONSENT</h2>
+<p>By using our site, you consent to our website Privacy Policy and Website Terms.
+</p>
+<h2>CHANGES TO OUR PRIVACY POLICY</h2>
+<p>This policy was last updated on the date shown at the top of this policy. The Project reserves the right to change, modify or amend this policy at will and without notice. We therefore encourage you to review this Policy regularly to inform yourself about how your Personal Information is collected, used and shared by the Project. To the fullest extent allowed under Federal law, your use of our website after the Effective date means that you are subject to the Policy then in effect.</p>
+
+<h2>CONTACTING US</h2>
+<p>Please contact the Project regarding any privacy concerns or questions you have via <a href="mailto:csthr@nd.edu">email</a>.
+</p>
+<h2>Terms and Conditions</h2>
+<p>This is the website of: the Catholic Social Teaching and International Human Rights Comparative Database Project (the “Project”), in the Center for Civil and Human Rights at the University of Notre Dame.</p>
+<p>Set out below are the rules that will apply to your use of our website. In using our website you are agreeing to comply with these rules including our Privacy Policy. We may amend the rules at any time by posting the amended rules on our website. All amended rules will apply from the time they are posted on our website. Your continued use of the website signifies your agreement to the terms contained herein at any time.</p>
+
+<h2>DATA PROTECTION NOTIFICATION</h2>
+<p>The website is operated by the Project. Please see our Privacy Policy for specific information relating to Data Protection.</p>
+
+<h2>PRIVACY POLICY (COOKIES)</h2>
+<p>We use cookies to enhance the functionality of the site.</p>
+
+<h2>INTELLECTUAL PROPERTY RIGHTS</h2>
+<p>The content and software on this site is to be used solely for providing information to users of this site.
+You may print material from the Project website for your legitimate purposes, but you may not charge a fee for any use and commercial use is expressly prohibited.</p>
+
+<h2>DISCLAIMER AND LIMITATION OF LIABILITY</h2>
+<p>Due to the changing nature of the information contained in this website, neither the Project, the University of Notre Dame, nor anyone affiliated with either of them shall be liable for any damage or loss resulting from information accessible via this website not being accurate, complete or current.</p>
+<p>Due to the nature of communications over the Internet and the inherent risks of the transfer of data over the Internet, neither the Project, the University of Notre Dame, nor anyone affiliated with the either of them shall be liable for any damage or loss whatsoever or howsoever resulting from viruses or other forms of malware or the corruption or loss of data or damage to software or hardware.</p>
+<p>While the Project will use reasonable endeavors to minimize any amount of downtime on the website, due to the intrinsic nature of websites, neither the Project, the University of Notre Dame, nor anyone affiliated with the either of them shall be liable for any damage or loss whatsoever resulting from your being unable to access the website.</p>
+<p>Without prejudice to the above, neither the Project, the University of Notre Dame, nor anyone affiliated with either of them shall be liable for any direct, indirect, special or consequential losses or damages whatsoever, howsoever arising (including but not limited to loss of business, loss of opportunity, loss of sale, loss of goodwill, loss of profits or increased costs).</p>
+<p>The disclaimers set out above do not affect or prejudice any other Project or University of Notre Dame disclaimers or exclusions relating to this website (whether or not contained in these terms).</p>
+
+<h2>TERMINATION </h2>
+<p>The Project may deny access to to any user or suspend or terminate any of the services offered herein, or delete the website at any time in its sole discretion.</p>
+
+<h2>HYPERLINKS</h2>
+<p>The Project website may provide hyperlinks to other websites. The Project is not responsible for the availability of such other websites and does not endorse, and is not responsible or liable for any content, products or other materials available on such other websites.</p>
+
+<h2>GENERAL</h2>
+<p>Any waiver or amendment of any of these rules will be effective only if signed by an authorized representative of the University of Notre Dame, and the failure of the Project or the University of Notre Dame to exercise or enforce any rule shall not constitute a waiver of such right or provision. The Project reserves the right to make changes to this website and these rules at any time in its sole discretion.</p>
+        	<p>&nbsp;</p>
+        	<p>&nbsp;</p>
+        	<p>&nbsp;</p>
+        	</div>
+        </div>
+        <Footer/>
+      </div>
+    );
+  }
+
+});
+
+module.exports = Privacy;
