1cff59fe93ea6942f57a516c4299b53ab8518057
Dan Wolfe <dwolfe2@nd.edu>
2016-05-05 15:22:43 -0400

    'fix'

--- src/components/StaticAssets/Footer.jsx
+++ src/components/StaticAssets/Footer.jsx
@@ -14,8 +14,8 @@ class Footer extends Component {
             { this.props.showBackToSearchButton ? (<BackToSearchButton/>) : '' }
           </div>
           <div className="col-sm-4" style={{textAlign: 'center', zIndex: '6'}} >
-            <a href="https://docs.google.com/a/nd.edu/forms/d/1yCnSjl4nBCJYmw70_S2VfVx1LzgNQ-kmroOqapq6i0Q/viewform"  className="feedback" target="_blank">Project Feedback<br /></a>
-            <span className="tearms"><a href="#">Terms of Service</a> | <a href="/Privacy">Privacy Policy</a></span>
+            <Link to="https://docs.google.com/a/nd.edu/forms/d/1yCnSjl4nBCJYmw70_S2VfVx1LzgNQ-kmroOqapq6i0Q/viewform"  className="feedback" target="_blank">Project Feedback<br /></Link>
+            <span className="tearms"><a href="#">Terms of Service</a> | <Link to="/privacy">Privacy Policy</Link></span>
           </div>
           <div className="col-sm-4 right">
             <Link to="http://humanrights.nd.edu/" ><img src="/resources/images/cchr.png" alt="The Center For Civil and Human Rights" className="cchr" /></Link>
