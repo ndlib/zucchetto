7d705bc32e6a0d3a0ac0d46683dc3e53de907bb8
Dan Wolfe <dwolfe2@nd.edu>
2016-05-31 14:44:30 -0400

    Fix using the database link

--- src/components/StaticPages/SiteIndex.jsx
+++ src/components/StaticPages/SiteIndex.jsx
@@ -51,7 +51,7 @@ var SiteIndex = React.createClass({
         <Header />
       		<section className="search">
             <h2><Link to="/search?q=">Search The Database <i className="glyphicon glyphicon-circle-arrow-right"></i></Link></h2>
-            <p><Link to="/search?q=">Using the Database</Link></p>
+            <p><Link to="/using">Using the Database</Link></p>
           </section>
 
         <HomePageNavigation/>
