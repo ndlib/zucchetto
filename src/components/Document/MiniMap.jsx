0e6f8cfa20a886f8773afef35be3e8569ac3e575
Dan Wolfe <dwolfe2@nd.edu>
2016-06-07 09:49:13 -0400

    inprogress

--- src/components/Document/MiniMap.jsx
+++ src/components/Document/MiniMap.jsx
@@ -6,10 +6,10 @@ class MiniMap extends Component {
 
   constructor(props) {
     super(props);
-    this._scale = .2;
     this.scrollToHere = this.scrollToHere.bind(this);
     this.setTop = this.setTop.bind(this);
     this.state = {
+      scale: .2,
       width: 0,
       height: 0,
     }
@@ -17,9 +17,11 @@ class MiniMap extends Component {
 
   componentDidMount() {
     this.setState({
-      width: ReactDOM.findDOMNode(this.refs.miniMap).clientWidth/this._scale,
-      height: ReactDOM.findDOMNode(this.refs.miniMap).parentElement.clientHeight/this._scale,
+      width: ReactDOM.findDOMNode(this.refs.miniMap).clientWidth / this.state.scale,
+      height: ReactDOM.findDOMNode(this.refs.miniMap).parentElement.clientHeight / this.state.scale,
+      top: 0
     });
+
   }
 
   componentDidUpdate() {
@@ -27,16 +29,18 @@ class MiniMap extends Component {
   }
 
   setTop() {
-    ReactDOM.findDOMNode(this.refs.miniMap).parentElement.scrollTop = (this.props.scrollTop - 40) * this._scale;
+    ReactDOM.findDOMNode(this.refs.miniMap).parentElement.scrollTop = (this.props.scrollTop - 40) * this.state.scale;
   }
 
   scrollToHere(e){
+    var here = (ReactDOM.findDOMNode(this.refs.miniMap).parentElement.scrollTop + e.screenY - 210)/this.state.scale;
     e.preventDefault();
-    this.props.onClick((ReactDOM.findDOMNode(this.refs.miniMap).parentElement.scrollTop + e.screenY - 210)/this._scale);
+    this.props.onClick(here);
+    this.setState({ top: (ReactDOM.findDOMNode(this.refs.miniMap).parentElement.scrollTop + e.screenY - 290)/this.state.scale })
   }
 
   style() {
-    var s = { x: this._scale, y: this._scale };
+    var s = { x: this.state.scale, y: this.state.scale };
     var sc = 'scale(' + s.x + ','+ s.y + ')';
     return {
       transform: sc,
@@ -47,6 +51,18 @@ class MiniMap extends Component {
   render() {
     return (
       <div style={ this.style()} className='mini-map' ref='miniMap' onClick={ this.scrollToHere } >{this.props.children}
+        <div
+          className="mini-map-highlight"
+          style={{
+            backgroundColor: 'rgba(255, 165, 0, 0.38)',
+            height: window.innerHeight - 100,
+            width: window.innerWidth * .65,
+            position: 'absolute',
+            top: this.state.top + 330,
+            right: '-300%',
+            zIndex: '1',
+          }}
+        >&nbsp;</div>
       </div>
     );
   }
