d41e42ab25ec709d860f2edc2e20ea1bf88865ad
Dan Wolfe <dwolfe2@nd.edu>
2016-06-07 11:14:21 -0400

    Need to add Link, linkButton is a new feature of Material UI 15 and not supported in 14.

--- src/components/StaticAssets/FeedbackLink.jsx
+++ src/components/StaticAssets/FeedbackLink.jsx
@@ -1,21 +1,29 @@
 'use strict'
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 import mui, { FlatButton } from "material-ui"
 
 class FeedbackLink extends Component {
 
   render() {
     return (
+      <Link to="https://docs.google.com/a/nd.edu/forms/d/1yCnSjl4nBCJYmw70_S2VfVx1LzgNQ-kmroOqapq6i0Q/viewform" target="_blank">
       <FlatButton
         backgroundColor="#E4E1D1"
         hoverColor="#D5B117"
         className="feedback"
-        style={{ 	transform: "rotate(90deg)", transformOrigin: "right top 0", position: "absolute", right: 0, bottom: 150, padding: "0px 15px" }}
-        href="https://docs.google.com/a/nd.edu/forms/d/1yCnSjl4nBCJYmw70_S2VfVx1LzgNQ-kmroOqapq6i0Q/viewform"
-        target="blank"
+        style={{
+          transform: "rotate(90deg)",
+          transformOrigin: "right top 0",
+          position: "absolute",
+          right: 0,
+          bottom: 150,
+          padding: "0px 15px"
+        }}
         >
         Feedback
       </FlatButton>
+    </Link>
     );
   }
 
