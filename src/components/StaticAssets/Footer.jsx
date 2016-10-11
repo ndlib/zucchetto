3ec4a2f15b3a6a42447a04264aeb8e7685f19eb3
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-10-11 14:32:36 -0400

    restore header and footer

--- src/components/StaticAssets/Footer.jsx
+++ src/components/StaticAssets/Footer.jsx
@@ -4,24 +4,7 @@ import Drawer from '../../components/Search/Drawer.jsx';
 
 class Footer extends Component {
   render() {
-    return (
-      <footer>
-        <div className="row">
-          <div className="col-sm-4 compare">
-            { this.props.showCompareButton ? (<CompareButton/>) : '' }
-            { this.props.showBackToSearchButton ? (<BackToSearchButton/>) : '' }
-          </div>
-          <div className="col-sm-4" style={{textAlign: 'center', zIndex: '6'}} >
-            <Link to="https://docs.google.com/a/nd.edu/forms/d/1yCnSjl4nBCJYmw70_S2VfVx1LzgNQ-kmroOqapq6i0Q/viewform"  className="feedback" target="_blank">Report a Problem<br /></Link>
-            <span className="tearms"><Link to='/terms'>Terms of Service</Link> | <Link to="/privacy">Privacy Policy</Link></span>
-          </div>
-          <div className="col-sm-4 right">
-            <Link to="http://humanrights.nd.edu" target="_blank" ><img src="/resources/images/cchr.png" alt="The Center For Civil and Human Rights" className="cchr" /></Link>
-            <Link to="https://library.nd.edu/cds" target="_blank" ><img src="/resources/images/undhl-cds.png" alt="Hesburgh Library Center for Digital Scholarship University of Notre Dame" className="undhl" /></Link>
-          </div>
-        </div>
-      </footer>
-    );
+    return (<Drawer />);
   }
 }
 
--- src/components/StaticAssets/Header.jsx
+++ src/components/StaticAssets/Header.jsx
@@ -70,10 +70,19 @@ class Header extends Component {
             <Link to="https://docs.google.com/a/nd.edu/forms/d/1yCnSjl4nBCJYmw70_S2VfVx1LzgNQ-kmroOqapq6i0Q/viewform"  className="feedback" target="_blank" style={{ backgroundColor: "white", textAlign: "center" }}>Project Feedback<br /></Link>
             <span className="tearms"><Link to='/terms'>Terms of Service</Link> | <Link to="/privacy">Privacy Policy</Link></span>
           </div>
-        </div>
-        <div className="feedback"><a href="https://docs.google.com/a/nd.edu/forms/d/1yCnSjl4nBCJYmw70_S2VfVx1LzgNQ-kmroOqapq6i0Q/viewform" target="_blank">Report a Problem</a></div>
+          <p
+            style={{
+                position: 'fixed',
+                bottom: '0',
+                margin: '10px',
+            }}
+          >
+            <a href="http://humanrights.nd.edu/" target="_blank" ><img src="/resources/images/cchr.png" alt="The Center For Civil and Human Rights" className="cchr m" /></a>
+            <a href="https://library.nd.edu/cds" target="_blank" ><img src="/resources/images/undhl-cds.png" alt="Hesburgh Library Center for Digital Scholarship University of Notre Dame" className="cds" style={{ marginTop: '10px'}} /></a>
+          </p>
+        </LeftNav>
+      </div>
 
-      </header>
     );
   }
 }
