529a1d9d26ce52e8e493d0fb7c632a276d4712a3
Harrison Beachey <hbeachey@nd.edu>
2016-11-07 12:56:23 -0500

    deployment to pprd requires a different user profile now

--- awsbuild_pprd.sh
+++ awsbuild_pprd.sh
@@ -12,6 +12,6 @@ cd ./public
 curl -o ./resources/cache_data/cst_data.json ${HONEYCOMB}/v1/collections/vatican/items
 curl -o ./resources/cache_data/ihrl_data.json ${HONEYCOMB}/v1/collections/humanrights/items
 
-aws s3 sync . s3://${BUCKET} --exclude '.*' --exclude '*.md' --delete --acl public-read
+aws s3 sync . s3://${BUCKET} --exclude '.*' --exclude '*.md' --delete --acl public-read --profile "testlibnd-superAdmin"
 echo "\033[0;31mDeployed to ${BUCKET}.s3-website-us-east-1.amazonaws.com. Rebuilding for development.\033[0m"
 npm run build-dev-nowatch
