11cb0bf88482979fd3b792c528e5c7dfa869675f
Harrison Beachey <hbeachey@nd.edu>
2017-01-05 14:24:20 -0500

    add alternate title to the document index

--- src/components/Document/DocumentLink.jsx
+++ src/components/Document/DocumentLink.jsx
@@ -12,7 +12,11 @@ class DocumentLink extends Component {
   }
 
   render() {
-    return (<li><Link to={this.documentURL(this.props.item)}>{this.props.item.name}</Link></li>);
+    var item = this.props.item;
+    var alternate = item.metadata.alternative_title ? "\n" + item.metadata.alternative_title.values[0].value : "";
+    return (<li><Link to={this.documentURL(item)}>{item.name}</Link>
+          <div className="subtitle"><p>{alternate}</p></div>
+      </li>);
   }
 }
 
