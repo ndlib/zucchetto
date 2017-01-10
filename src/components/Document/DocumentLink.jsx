f8493f682f8bb4857d4af5345fa175b3a04dd86f
Harrison Beachey <hbeachey@nd.edu>
2017-01-10 15:12:52 -0500

    remove unnessasary newline

--- src/components/Document/DocumentLink.jsx
+++ src/components/Document/DocumentLink.jsx
@@ -13,7 +13,7 @@ class DocumentLink extends Component {
 
   render() {
     var item = this.props.item;
-    var alternate = item.metadata.alternative_title ? "\n" + item.metadata.alternative_title.values[0].value : "";
+    var alternate = item.metadata.alternative_title ? item.metadata.alternative_title.values[0].value : "";
     return (<li><Link to={this.documentURL(item)}>{item.name}</Link>
           <div className="subtitle"><p>{alternate}</p></div>
       </li>);
