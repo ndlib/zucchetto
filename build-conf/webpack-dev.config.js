f04f332bc71879303c4401c893c653f6659e2112
Dan Wolfe <dwolfe2@nd.edu>
2017-11-10 14:36:35 -0500

    React 15 -> 16 Update
    * Update Node
    * Update Webpack
    * Update React

--- build-conf/webpack-dev.config.js
+++ build-conf/webpack-dev.config.js
@@ -1,34 +1,30 @@
 var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path')
 
 module.exports = [
   {
     name: "browser",
     entry: {
-        main: './src/main.js'
+        main: path.join(__dirname, '../src/main.js')
     },
     output: {
-        path: './public',
+        path: path.join(__dirname, '../public'),
         filename: 'resources/bundle.js'
     },
     devtool: 'inline-source-map',
-    debug: true,
+    //debug: true,
     module: {
-      loaders: [
+      rules: [
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
-          loader: 'babel',
+          loader: 'babel-loader',
           query: {
             cacheDirectory: true,
             presets: ['react', 'es2015', 'stage-0'],
             plugins: ['transform-runtime', 'add-module-exports']
           }
-        },
-        {
-          test: /\.(css|scss)$/,
-          exclude: /node_modules/,
-          loader: ExtractTextPlugin.extract("style", "css!sass")
         },
         {
           test: /\.(svg|png|jpg|gif)([\?]?.*)$/,
@@ -39,11 +35,23 @@ module.exports = [
           test: /\.(eot|woff|woff2|ttf)([\?]?.*)$/,
           exclude: /node_modules/,
           loader: 'url-loader'
-        }
-      ]
+        },
+        {
+        test: /\.(css|scss)$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      }]
     },
     plugins: [
       new ExtractTextPlugin("resources/styles.css"),
+      new webpack.LoaderOptionsPlugin({
+        debug: true
+      }),
       new webpack.DefinePlugin({
         "process.env": {
           NODE_ENV: JSON.stringify("development")
